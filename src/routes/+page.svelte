7b0736776ed15d1644273fbdae4ce71f2037cda1
DanBaDo <byte4life@pm.me>
2023-03-07 13:00:11 +0100

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div id="envoltorio">
+    <h1>Contador</h1>
+    <div>{numero}</div>
+    <Boton texto="Suma" accion={manexadorClickSuma}/>
+    <Boton texto="Resta" accion={manexadorClickResta}/>
+</div>
+
+<script>
+
+    import Boton from "./Boton.svelte";
+
+    let numero = 0
+
+    function manexadorClickSuma(){
+        numero++
+    }
+
+    function manexadorClickResta(){
+        numero--
+    }
+
+</script>
+
+<style>
+
+</style>
---
+++ src/routes/Boton.svelte
@@ -0,0 +1,12 @@
+<button on:click={manexadorClick}>{texto} ({numero})</button>
+
+<script>
+    export let texto
+    export let accion
+    let numero = 0
+
+    function manexadorClick() {
+        numero++
+        accion()
+    }
+</script>
